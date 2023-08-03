8410ba5dad2af402dbacf99d3df085386c48b96a
hena-ndombele <henandombele2021@gmail.com>
2023-08-03 15:22:01 +0100

    Ajout de la page apropos

--- lib/Pages/Page/AproposPage.dart
+++ lib/Pages/Page/AproposPage.dart
@@ -13,14 +13,25 @@ class _AproposPageState extends State<AproposPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+appBar: AppBar(
+  title: Text("A propos"),
+  backgroundColor: Utils.COLOR_VIOLET,
+),
 
-
-      body: Column(
-        children: [
-          Text("A propos"),
-
-          Text("version actuelle : 1.0.0+1",style: TextStyle(color: Utils.COLOR_GREY),)
-        ],
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              SizedBox(height: 50,),
+             Image.asset(
+                'images/Capture12.PNG',width: 100,
+              ),
+              SizedBox(height: 10,),
+              Text("version actuelle : 1.0.0",style: TextStyle(color: Utils.COLOR_GREY,fontSize: 17),)
+            ],
+          ),
+        ),
       ),
 
     );
